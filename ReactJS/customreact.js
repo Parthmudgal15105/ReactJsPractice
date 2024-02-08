60e85fa817f835a498e99dbbfcd41ba5777abb1a
Parthmudgal15105 <parthmudgal15105@gmail.com>
2024-02-08 18:53:29 +0530

    first commit

---
+++ ReactJS/customreact.js
@@ -0,0 +1,9 @@
+const reactElelment ={
+    type: 'a',
+    Props: {
+        href: 'https://google.com',
+        target: '_blank'
+    }
+}
+
+const mainContainer = document.querySelector('#root')
